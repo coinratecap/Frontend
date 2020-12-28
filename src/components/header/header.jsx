5499806c5e8da7f6816ece930fe6d551f4b77a1d
justinepeterson <jmahinyila@gmail.com>
2020-12-28 17:45:09 +0300

    Header with logo, navigation menu and signup button

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,9 +1,48 @@
 import "./header.sass";
-
+import logo from "../../assets/images/logo.png";
 function App() {
-	return <div className="header">
-        Header
-    </div>;
+	return (
+		<div className="header">
+			<div className="logo">
+				<a href="http://localhost:3000/">
+					<img src={logo} alt="The coinratecap logo" />
+				</a>
+			</div>
+			<div className="nav">
+				<ul>
+					<li>
+						<a href="http://localhost:3000/">Home</a>
+					</li>
+					<li>
+						<a href="http://localhost:3000/">Markets</a>
+					</li>
+					<li>
+						<a href="http://localhost:3000/">Tools</a>
+					</li>
+					<li>
+						<a href="http://localhost:3000/">Blog</a>
+					</li>
+					<li>
+						<a href="http://localhost:3000/">Resorces</a>
+					</li>
+					<li>
+						<a href="http://localhost:3000/">Portfolio</a>
+					</li>
+					<li>
+						<a href="http://localhost:3000/">Events</a>
+					</li>
+					<li>
+						<a href="http://localhost:3000/">Community</a>
+					</li>
+				</ul>
+			</div>
+			<div className="signup">
+				<button>
+					<a href="http://localhost:3000/">Sign Up</a>
+				</button>
+			</div>
+		</div>
+	);
 }
 
 export default App;
