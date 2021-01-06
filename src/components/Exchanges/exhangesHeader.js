6f891f2bd4ad2e0c5a2151d7db6ed01c91a12882
wisdom Ekpot <wisdomekpot@gmail.com>
2021-01-06 13:22:28 +0100

    fixed classname

--- src/components/Exchanges/exhangesHeader.js
+++ src/components/Exchanges/exhangesHeader.js
@@ -3,10 +3,10 @@ import './exchange.css'
 
 const ExchangeHeader = (prop) => {
     return (
-        <section id="exchange-header" class="d-flex align-items-end">
-            <div class="container">
-                <div class="row bg-wrapper">
-                    <div class="col-md-8 offset-md-2">
+        <section id="exchange-header" className="d-flex align-items-end">
+            <div className="container">
+                <div className="row bg-wrapper">
+                    <div className="col-md-8 offset-md-2">
                         <h3>{prop.title}</h3>
                     </div>
                 </div>
